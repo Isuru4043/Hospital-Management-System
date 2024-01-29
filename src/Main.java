d21b6152816f66ceb693b8a9b1013c82b91fbaac
Isuru <isuru.akalanka4043@gmail.com>
2024-01-29 13:36:43 +0530

    Revert "initialize the login form"
    This reverts commit ab0add1359442252be0209b6a873417fe6490fad.

--- src/Main.java
+++ src/Main.java
@@ -260,8 +260,7 @@ public class Main extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-        User u = new User();
-        u.setVisible(true);
+        // TODO add your handling code here:
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
